549e5282f4b2a40730467579920fc97513f6b998
Allan Jiang <origami789@gmail.com>
2018-02-03 18:54:03 -0800

    updated assignment 2

--- README.md
+++ README.md
@@ -1,36 +1,106 @@
-# pset2
+# CS43: PSET 2
 
-FIXME: description
+Practice with Macros!
 
-## Installation
+## Instructions
 
-Download from http://example.com/FIXME.
+1. Clone this repo
+1. submit your `src/pset2` folder as a tarball or zip file to
+   `jiangts@stanford.edu` by February 13, 2018
 
-## Usage
+## Problems
 
-FIXME: explanation
+1. Write the macro `unless`
 
-    $ java -jar pset2-0.1.0-standalone.jar [args]
+the syntax for `if` is
+`(if <condition> <true-expr> <false-exp>)`
+where the true-expr is evaluated only if condition is truthy,
+and the false-expr is evaluated only if condition is falsey
+the syntax for `unless` is
+`(unless <condition> <false-expr> <true-expr>)`
 
-## Options
+2. Write the macro `from` to implement a [LINQ](linq) inspired syntax.
 
-FIXME: listing of options this app accepts.
 
-## Examples
+```clojure
+(def names ["Burke", "Connor", "Frank", "Everett", "Albert", "George", "Harris", "David"])
 
-...
+(from n in names
+      (where (= (count n) 5))
+      (orderby n)
+      (select (.toUpperCase n))) => '("BURKE" "DAVID" "FRANK")
+```
 
-### Bugs
+As you see, you can bind the symbol `n` to a value from runtime (names), and you
+can pass it an arbitrary set of query clauses that transform the inputs.
+Some transformations, like `select`, can specify function-esque arguments to
+further transform the collection.
+You only need to implement `where`, `orderby` and `select` for this assignment.
 
-...
+3. Write the macro `with-file`, with usage:
 
-### Any Other Sections
-### That You Think
-### Might be Useful
+```clojure
+(with-file "project.clj"
+  (if (.hasNextLine file)
+    (.nextLine file)))
+```
 
-## License
+where the file object is bound to the `file` symbol within the macro body, and
+is automatically closed when control flow exits the expression.
 
-Copyright © 2018 FIXME
+4. What's the bug in the `square` macro, intended to pre-compute the square of
+   the input?
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+```clojure
+(defmacro square [n]
+  `(* ~n ~n))
+```
+
+5. Find the bug and fix the following numeric-if "`nif`" macro:
+
+```clojure
+(defmacro nif [val pos zero neg]
+  "Numeric if. Executes pos, zero or neg depending
+  if val is positive, zero or negative respectively"
+  `(let [~'res ~val]
+     (cond (pos? ~'res) ~pos
+           (zero? ~'res) ~zero
+           :else ~neg)))
+```
+
+## Bonus Project
+
+We introduced `clojure.spec` in class.
+We talked about how it dovetails nicely with macros to help validate macro
+inputs and give robust error messages.
+
+We can also use specs to create alternative interfaces to an API via `conform`
+and `unform`! Wouldn't it be nice if we created a configuration-driven API to
+WebRTC?
+
+Let's do that, by creating specs for WebRTC!
+Sounds cumbersome?
+
+
+... Macros to the rescue!
+
+Macros transform input data, which may or may not be code, into output data,
+which is interpreted as code. It turns out that the input data need not be
+Clojure forms, it can just be data from the w3 specs website!
+
+In `src/pset2/rtc.clj` you will find some starter code that scrapes all of the
+Interface Definition Language (IDL) specification data out of the official
+WebRTC spec and parses into simple data structures you can use to output specs
+for WebRTC. These specs will help us create a more data-driven and
+Clojure-friendly interface to WebRTC.
+
+
+#### Example IDL and corresponding spec:
+See the comment on the bottom of `src/pset2/rtc.clj`
+
+[linq]: https://en.wikipedia.org/wiki/Language_Integrated_Query
+
+
+
+## References
+https://github.com/leonardoborges/clojure-macros-workshop
--- src/pset2/rtc.clj
+++ src/pset2/rtc.clj
@@ -1,12 +1,13 @@
 (ns pset2.rtc
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as string]
-            [instaparse.core :as insta]
             [clojure.spec.alpha :as s]))
 
 
 (defn- delete-last [s]
   (string/join "" (drop-last s)))
+
+
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
@@ -19,6 +20,8 @@
       (string/split #":"))
     (map string/trim)
     (map #(string/split % #"\s"))))
+
+
 (defn fields [b]
   (->>
     (-> (re-find #"\{(.*)\}" b)
@@ -27,6 +30,8 @@
       (string/split #";"))
     (map string/trim)
     (map #(string/split % #"\s"))))
+
+
 (defn process-raw-spec [raw-spec]
   (let [normalized (-> raw-spec
                      (string/replace #"[\n\t]" "")
@@ -35,50 +40,60 @@
     {:header (header normalized)
      :fields (fields normalized)}))
 
+(def url "https://www.w3.org/TR/webrtc/")
+(def w3c-html (fetch-url url))
+(def idl-text (map html/text (html/select w3c-html [:.def.idl :> html/any-node])))
+
+
+(process-raw-spec (println (nth idl-text 0)))
+
 
 (comment
-  ;; validate www.w3.org
-  (def url "https://www.w3.org/TR/webrtc/")
-  ;; (def w3c-spec-string (slurp url))
-  ;; (println w3c-spec-string)
-  (def w3c-html (fetch-url url))
-  (println w3c-html)
-  (def text-specs (map html/text (html/select w3c-html [:.def.idl :> html/any-node])))
-  (process-raw-spec (nth text-specs 4))
-
-  (read-string "[(dictionary RTCErrorEventInit) (EventInit) [ (RTCError? error = null) ]]")
+  "Example IDL and corresponding spec:"
 
 
+  "dictionary RTCConfiguration {
+      sequence<RTCIceServer>   iceServers;
+      RTCIceTransportPolicy    iceTransportPolicy = \"all\";
+      RTCBundlePolicy          bundlePolicy = \"balanced\";
+      RTCRtcpMuxPolicy         rtcpMuxPolicy = \"require\";
+      DOMString                peerIdentity;
+      sequence<RTCCertificate> certificates;
+      [EnforceRange]
+      octet                    iceCandidatePoolSize = 0;
+  };"
 
-  (def parser (insta/parser (slurp "resources/spec-parser.txt")))
+  ;; we don't understand the type of RTCIceServer from this example
+  (s/def ::RTCIceServer ???)
+  ;; we can only populate a subset of this enum using this single example
+  ;; it's defined later, and we see it's an enum containing #{"all" "relay"}
+  (s/def ::RTCIceTransportPolicy #{"all"})
+  (s/def ::RTCBundlePolicy #{"balanced"})
+  (s/def ::RTCRtcpMuxPolicy #{"require"})
+  (s/def ::DOMString string?)
+  (s/def ::RTCCertificate ???)
+  (s/def ::octet ???)
 
+  (s/def ::iceServers (s/* ::RTCIceServer))
+  (s/def ::iceTransportPolicy ::iceTransportPolicy)
+  (s/def ::bundlePolicy ::bundlePolicy)
+  (s/def ::rtcpMuxPolicy ::RTCRtcpMuxPolicy)
+  (s/def ::peerIdentity ::DOMString)
+  (s/def ::certificates (s/* ::RTCCertificate))
+  (s/def ::iceCandidatePoolSize ::octet)
 
-  (def transform-options
-    {})
+  (def defaults {::iceTransportPolicy "all"
+                 ::bundlePolicy "balanced"
+                 ::rtcpMuxPolicy "require"
+                 ::iceCandidatePoolSize 0})
 
-  (defn parse [s]
-    (->> (parser s)
-      (insta/transform transform-options)))
+  (s/def ::RTCConfiguration
+    (s/keys :opt [::iceServers
+                  ::iceTransportPolicy
+                  ::bundlePolicy
+                  ::rtcpMuxPolicy
+                  ::peerIdentity
+                  ::certificates
+                  ::iceCandidatePoolSize]))
 
-  (parse (first text-specs))
-
-
-
-  (defn choose-operator [op]
-    (case op
-      "+" +
-      "-" -
-      "*" *
-      "/" /))
-
-
-  (def transform-options
-    {:number read-string
-     :vector vector
-     :operator choose-operator
-     :operation apply
-     :expr identity})
   )
-
-
-
